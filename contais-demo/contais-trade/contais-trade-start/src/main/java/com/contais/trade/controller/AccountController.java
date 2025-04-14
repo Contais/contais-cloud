1e8f16fc0d39d27daafbd9a37ac69815a75a9d2f
lham <523800546@qq.com>
2025-04-14 09:44:53 +0800

    feat: contais-trade集成nacos作为配置中心的demo

--- contais-demo/contais-trade/contais-trade-start/src/main/java/com/contais/trade/controller/AccountController.java
+++ contais-demo/contais-trade/contais-trade-start/src/main/java/com/contais/trade/controller/AccountController.java
@@ -2,20 +2,29 @@ package com.contais.trade.controller;
 
 import com.contais.trade.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
+@RefreshScope
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
+    @Value("${contais.config.test:abc}")
+    private String configTest;
+
     @PostMapping("/debit")
     public boolean debit(@RequestParam("userId") String userId, @RequestParam("money") int money) {
         return accountService.debit(userId, money);
     }
+
+    @GetMapping("/test")
+    public String test() {
+        return configTest;
+    }
+
 }
